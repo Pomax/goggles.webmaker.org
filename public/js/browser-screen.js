f8d5d8150f71e15393665e83a2c29531d81a1cb7
admix <admix.snurnikov@gmail.com>
2014-03-25 23:58:09 -0400

    [bug968291] - Added GA events to Goggles

--- public/js/browser-screen.js
+++ public/js/browser-screen.js
@@ -1,43 +1,44 @@
 (function() {
   "use strict";
 
-  var hostname = document.getElementById("browser-screen").getAttribute("data-hostname");
-  var localeInfo = document.getElementById("browser-screen").getAttribute("data-localeInfo");
+  var hostname = document.getElementById("browser-screen").getAttribute("data-hostname"),
+      localeInfo = document.getElementById("browser-screen").getAttribute("data-localeInfo");
 
   $(window).ready(function() {
+    require(["jquery", "bower/webmaker-analytics/analytics.js"], function($, analytics) {
+      $("#bookmarklet-link").attr("href", Webxray.getBookmarkletURL(hostname, localeInfo));
 
-    $("#bookmarklet-link").attr("href", Webxray.getBookmarkletURL(hostname, localeInfo));
-
-    $("#bookmarklet-link").on("click", function(event) {
-      event.preventDefault();
-      var script = document.createElement('script');
-      script.src = '/webxray.js';
-      script.className = 'webxray';
-      script.setAttribute('data-lang',localeInfo);
-      script.setAttribute('data-baseuri', hostname + "/"+localeInfo);
-      document.body.appendChild(script);
+      $("#bookmarklet-link").on("click", function(event) {
+        analytics.event("Activate X-Ray Goggles");
+        event.preventDefault();
+        var script = document.createElement('script');
+        script.src = '/webxray.js';
+        script.className = 'webxray';
+        script.setAttribute('data-lang',localeInfo);
+        script.setAttribute('data-baseuri', hostname + "/"+localeInfo);
+        document.body.appendChild(script);
+      });
     });
+      // browser-specific screenshots
+      // http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+      var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+          // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+      var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+      var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+          // At least Safari 3+: "[object HTMLElementConstructor]"
+      var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+      var isIE = /*@cc_on!@*/false || document.documentMode;   // At least IE6
+      var browser = "firefox";
 
-    // browser-specific screenshots
-    // http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-    var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-        // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
-    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-        // At least Safari 3+: "[object HTMLElementConstructor]"
-    var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
-    var isIE = /*@cc_on!@*/false || document.documentMode;   // At least IE6
-    var browser = "firefox";
+      if (isChrome) {
+        browser = "chrome";
+      } else if (isSafari) {
+        browser = "safari";
+      }
 
-    if (isChrome) {
-      browser = "chrome";
-    } else if (isSafari) {
-      browser = "safari";
-    }
-
-    $('#install .screenshot.step1 img').attr('src', 'img/instructions/' + browser + '1.png');
-    $('#install .screenshot.step2 img').attr('src', 'img/instructions/' + browser + '2.png');
-    $('.screenshot').fadeIn();
-  });
+      $('#install .screenshot.step1 img').attr('src', 'img/instructions/' + browser + '1.png');
+      $('#install .screenshot.step2 img').attr('src', 'img/instructions/' + browser + '2.png');
+      $('.screenshot').fadeIn();
+    });
 
 }());
--- webxray/src/touch-toolbar.js
+++ webxray/src/touch-toolbar.js
@@ -1,57 +1,58 @@
 (function(jQuery) {
   "use strict";
+  require(["jquery", "analytics"], function($, analytics) {
 
-  var $ = jQuery;
-
-  function canBeTouched() {
-    return ('ontouchstart' in window);
-  }
-
-  function makeButton(glyph, text, cb) {
-    var button = $(
-      '<div class="webxray-toolbar-button">' +
-      '<div class="webxray-toolbar-button-glyph"></div>' +
-      '<div class="webxray-toolbar-button-text"></div>' +
-      '</div>'
-      );
-    var glyphDiv = $('.webxray-toolbar-button-glyph', button);
-    glyphDiv.text(glyph);
-    if (glyph.length != 1)
-      glyphDiv.addClass('webxray-toolbar-button-glyph-tiny');
-    $('.webxray-toolbar-button-text', button).text(text);
-    button.find('*').andSelf().addClass('webxray-base');
-    button.bind('touchstart touchmove click', function(event) {
-      event.preventDefault();
-      cb.call(this);
-    });
-    return button;
-  }
-
-  jQuery.extend({
-    touchToolbar: function(input, locale, platform) {
-      locale = locale || jQuery.locale;
-      platform = platform || navigator.platform;
-
-      var toolbar = $('<div class="webxray-base webxray-toolbar"></div>');
-
-      input.keyboardHelp.forEach(function(binding) {
-        if (binding.execute && (canBeTouched() || binding.alwaysInToolbar))
-          makeButton(jQuery.nameForKey(binding.key, locale, platform),
-                     Localized.get("short-command-descriptions:" + binding.cmd), function() {
-                       binding.execute();
-                     }).appendTo(toolbar);
-      });
-
-      toolbar.appendTo(document.body);
-      input.on('activate', function() { toolbar.fadeIn(); });
-      input.on('deactivate', function() { toolbar.fadeOut(); });
-
-      return {
-        unload: function() {
-          toolbar.remove();
-          toolbar = null;
-        }
-      };
+    function canBeTouched() {
+      return ('ontouchstart' in window);
     }
+
+    function makeButton(glyph, text, cb) {
+      var button = $(
+        '<div class="webxray-toolbar-button">' +
+        '<div class="webxray-toolbar-button-glyph"></div>' +
+        '<div class="webxray-toolbar-button-text"></div>' +
+        '</div>'
+        );
+      var glyphDiv = $('.webxray-toolbar-button-glyph', button);
+      glyphDiv.text(glyph);
+      if (glyph.length != 1)
+        glyphDiv.addClass('webxray-toolbar-button-glyph-tiny');
+      $('.webxray-toolbar-button-text', button).text(text);
+      button.find('*').andSelf().addClass('webxray-base');
+      button.bind('touchstart touchmove click', function(event) {
+        analytics.event(text.charAt(0).toUpperCase() + text.slice(1));
+        event.preventDefault();
+        cb.call(this);
+      });
+      return button;
+    }
+
+    jQuery.extend({
+      touchToolbar: function(input, locale, platform) {
+        locale = locale || jQuery.locale;
+        platform = platform || navigator.platform;
+
+        var toolbar = $('<div class="webxray-base webxray-toolbar"></div>');
+
+        input.keyboardHelp.forEach(function(binding) {
+          if (binding.execute && (canBeTouched() || binding.alwaysInToolbar))
+            makeButton(jQuery.nameForKey(binding.key, locale, platform),
+                       Localized.get("short-command-descriptions:" + binding.cmd), function() {
+                         binding.execute();
+                       }).appendTo(toolbar);
+        });
+
+        toolbar.appendTo(document.body);
+        input.on('activate', function() { toolbar.fadeIn(); });
+        input.on('deactivate', function() { toolbar.fadeOut(); });
+
+        return {
+          unload: function() {
+            toolbar.remove();
+            toolbar = null;
+          }
+        };
+      }
+    });
   });
 })(jQuery);
